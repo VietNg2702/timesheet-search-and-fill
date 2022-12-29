dac4e4861100416cb2f0e868845594703a961036
VietNg2702 <49213361+VietNg2702@users.noreply.github.com>
2022-12-29 09:56:18 +0000

    update

--- foward.php
+++
@@ -1,4 +0,0 @@
-<?php
-shell_exec("./run.sh");
-header('Location: https://vietng2702.github.io/timesheet-search-and-fill/index.html?success=true');
-?>
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python search-and-fill.py
