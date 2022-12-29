e50fe665ef5a67529e4e3e1451626edbca8691ac
VietNg2702 <49213361+VietNg2702@users.noreply.github.com>
2022-12-29 09:42:36 +0000

    update

---
+++ foward.php
@@ -0,0 +1,4 @@
+<?php
+shell_exec("/var/www/html/run.sh");
+header('Location: https://vietng2702.github.io/timesheet-search-and-fill/index.html?success=true');
+?>
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python search-and-fill.py
